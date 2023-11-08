3287101b724965c1830856851f982adda5e1bb84
Tarique-B-DevOps <tariqueatwork@gmail.com>
2023-11-08 12:36:42 +0000

    initial commit

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,21 @@
+package org.acme;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @ConfigProperty(name = "property.name")
+    String name;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return name;
+    }
+}
